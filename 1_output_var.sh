c1597f751c1c1b92d8af3efde3b6c49bcc62100c
belien <2782694792@qq.com>
2023-05-31 12:57:18 +0000

    fetch - output and variable

---
+++ 1_output_var.sh
@@ -0,0 +1,39 @@
+# https://www.runoob.com/linux/linux-shell.html
+
+# 1.1 first line, mark: interpret execute this script
+#! /bin/bash
+# 1.2 output
+echo "hello world"
+
+# 1.3 variable
+## no spaces, keyworks in bash cannot be used 
+## first character is not a number
+## PHP need the symbol, is $
+
+### 1.3.1 multiple ways of writing : $name ${name} 'a '$name' !' "a $name !"
+name_1=me
+_varname="me2"
+echo $name_1
+echo ${_varname}
+
+### 1.3.2 doubel quotes(\"\") can used escape symbol,but single quotes('') not used!"
+echo "doubel quotes(\"\") : $name_1 or ${_varname}_2 in test1"
+echo 'single quotes('') : $name_1 or ${_varname}_2 in test1'
+echo 'single quotes('') double : '$name_1' or '${_varname}_2' in test1'
+echo single quotes\(\'\'\) in var : '$name_1' or '${_varname}_2' in test1
+echo doubel quotes\(\"\"\) in var : "$name_1" or "${_varname}_2" in test1
+
+### 1.3.3 readonly 
+echo ""
+your_name="tom"
+echo "source : $your_name"
+readonly your_name
+your_name="alibaba"
+echo 'override : '$your_name''
+
+### 1.3.4 nset
+echo ''
+echo "delete the variable of your_name ..."
+unset your_name
+
+
