afebe729f320bab311e14059515f1e4fb1e42c73
belien <2782694792@qq.com>
2023-05-31 13:46:32 +0000

    fetch - string,ergodic output

--- 1_output_var.sh
+++ 1_output_var.sh
@@ -5,18 +5,35 @@
 # 1.2 output
 echo "hello world"
 
+## for print in current dir, command is ``, not ''
+echo 
+i=1
+for file in `ls .`; do
+    echo current dir have $i file: $file
+    i=$i+1
+done
+
+echo 
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
+
 # 1.3 variable
+# https://www.runoob.com/linux/linux-shell-variable.html
+
 ## no spaces, keyworks in bash cannot be used 
 ## first character is not a number
 ## PHP need the symbol, is $
 
 ### 1.3.1 multiple ways of writing : $name ${name} 'a '$name' !' "a $name !"
+echo 
 name_1=me
 _varname="me2"
 echo $name_1
 echo ${_varname}
 
 ### 1.3.2 doubel quotes(\"\") can used escape symbol,but single quotes('') not used!"
+echo 
 echo "doubel quotes(\"\") : $name_1 or ${_varname}_2 in test1"
 echo 'single quotes('') : $name_1 or ${_varname}_2 in test1'
 echo 'single quotes('') double : '$name_1' or '${_varname}_2' in test1'
@@ -24,7 +41,7 @@ echo single quotes\(\'\'\) in var : '$name_1' or '${_varname}_2' in test1
 echo doubel quotes\(\"\"\) in var : "$name_1" or "${_varname}_2" in test1
 
 ### 1.3.3 readonly 
-echo ""
+echo 
 your_name="tom"
 echo "source : $your_name"
 readonly your_name
@@ -32,7 +49,7 @@ your_name="alibaba"
 echo 'override : '$your_name''
 
 ### 1.3.4 nset
-echo ''
+echo 
 echo "delete the variable of your_name ..."
 unset your_name
 
---
+++ 2_string.sh
@@ -0,0 +1,34 @@
+## concatenate
+echo
+echo "double quotes concatenate"
+your_name="runoob"
+### used double quotes
+greeting="hello, "$your_name" !"
+greeting_1="hello, ${your_name} !"
+echo $greeting  $greeting_1
+
+### used single quotes
+echo
+echo 'single quotes concatenate'
+greeting_2='hello, '$your_name' !'
+greeting_3='hello, ${your_name} !'
+echo $greeting_2  $greeting_3
+
+## length and index
+echo 
+echo 'get length by '$your_name''
+echo length: ${#your_name}
+echo \[0\] length: ${#your_name[0]}
+
+## substring
+echo
+str="this is a shell partice, the number of first index is zero!"
+echo "number 10 through 15 :"
+echo ${str}
+echo "${str:10:5}"
+
+## find substring
+echo
+echo 'find first char index by custom symbol 'i' or 'o''
+str="runoob is a great site"; echo $str
+echo `expr index "$str" io`
